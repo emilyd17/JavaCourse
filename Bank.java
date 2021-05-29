196a08c3a7ad37482e1dc68f5dac8a9d0328f0d9
Emily Dayanghirang <emdhng@gmail.com>
2021-05-29 02:15:21 -0700

    changed access modifiers

--- Bank.java
+++ Bank.java
@@ -6,33 +6,75 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class Bank {
-    static BankAccount[] accounts = new BankAccount[1]; //This array will point to all the Bank.java.BankAccount objects
-    static Scanner scnr = new Scanner(System.in);
-    static int noOfAccs = 0;           //Keeps track of the total number of accounts
-    static DecimalFormat moneyFormat = new DecimalFormat("$0.00");
+class BankAccount {
+    private int accNbr;
+    private double balance;
 
-    public static void main(String[] args) {
-        bankMenu(); //call bankMenu
+    public BankAccount(int accNbr, double balance) {
+        // Set instance variables
+        this.accNbr = accNbr;
+        this.balance = balance;
     }
 
-    //bankMenu runs the main part of the program, until user selects 'Q'
-    static void bankMenu() {
-        int currentIndex = -1;	//This variable tells us whether we are pointing to a valid account
+    public int getAcc() {
+        // Return accountNumber
+        return accNbr;
+    }
+
+    public double getBalance() {
+        // Return balance
+        return balance;
+    }
+
+    public void deposit() {
+        // Add to deposit
+        double deposit;
+        System.out.print("Enter amount of deposit: ");
+        deposit = Bank.scnr.nextDouble();
+        balance += deposit;
+    }
+
+    public void withdraw () {
+        // Withdraw as long as there is still $1 in the account
+        double withdraw;
+        System.out.print("Enter amount to withdraw: ");
+        withdraw = Bank.scnr.nextDouble();
+        while ((balance - withdraw) < 1) {
+            System.out.println("\nYou are withdrawing too much: try again");
+            System.out.print("\nEnter amount to withdraw: ");
+            withdraw = Bank.scnr.nextDouble();
+        }
+        balance -= withdraw;
+    }
+}
+
+public class Bank {
+    protected static BankAccount[] accounts = new BankAccount[1]; // This array will point to all the Bank.java.BankAccount objects
+    protected static Scanner scnr = new Scanner(System.in);
+    private static int noOfAccs = 0;           // Keeps track of the total number of accounts
+    protected static DecimalFormat moneyFormat = new DecimalFormat("$0.00");
+
+    public static void main(String[] args) {
+        bankMenu(); // Call bankMenu
+    }
+
+    // bankMenu runs the main part of the program, until user selects 'Q'
+    public static void bankMenu() {
+        int currentIndex = -1;	// This variable tells us whether we are pointing to a valid account
         String userSelection;
         boolean quit = false;
 
         do {
-            //display menu and ask for user selection
+            // Display menu and ask for user selection
             printMenu(currentIndex);
             userSelection = scnr.next();
 
             switch(userSelection.toUpperCase()) {
-                case "O": //Open account
-                    //make sure you have enough space or double size OF accounts array
-                    //make sure the account number is not a duplicate. Assign array index to account
-                    //set the current index;
-                    //increment the number of accounts
+                case "O": // Open account
+                    // Make sure you have enough space or double size OF accounts array
+                    // Make sure the account number is not a duplicate. Assign array index to account
+                    // Set the current index;
+                    // Increment the number of accounts
 
                     accounts[noOfAccs] = openAcc(noOfAccs); //Appends new account
                     currentIndex = noOfAccs - 1;
@@ -41,8 +83,8 @@ public class Bank {
                         accounts = resize();
                     }
                     break;
-                case "D": //Deposit
-                    //deposit only if currentIndex is not -1. you are depositing into a particular account
+                case "D": // Deposit
+                    // Deposit only if currentIndex is not -1. you are depositing into a particular account
                     if (currentIndex != -1) {
                         accounts[currentIndex].deposit();
                     }
@@ -57,8 +99,8 @@ public class Bank {
                         }
                     }
                     break;
-                case "S": //Select account
-                    //look for account and if it exists, set currentIndex to it
+                case "S": // Select account
+                    // Look for account and if it exists, set currentIndex to it
                     if (noOfAccs > 0) {
                         currentIndex = selectAcc(currentIndex);
                     }
@@ -67,8 +109,8 @@ public class Bank {
                                 "Please open an account.");
                     }
                     break;
-                case "C": //Close account
-                    //if currentindex is not -1 close the account and reset currentIndex
+                case "C": // Close account
+                    // If currentindex is not -1 close the account and reset currentIndex
                     if (currentIndex != -1) {
                         closeAcc(currentIndex);
                         /* After closing an account,
@@ -91,8 +133,8 @@ public class Bank {
                         }
                     }
                     break;
-                case "W": //Withdraw
-                    //if current index is not -1, withdraw
+                case "W": // Withdraw
+                    // If current index is not -1, withdraw
                     if (currentIndex != -1) {
                         accounts[currentIndex].withdraw();
                     }
@@ -106,18 +148,18 @@ public class Bank {
                         }
                     }
                     break;
-                case "L": //List accounts
-                    //traverse through all the accounts and display their information
+                case "L": // List accounts
+                    // Traverse through all the accounts and display their information
                     if (noOfAccs > 0) {
                         listAccounts();
                     }
                     else {
                         System.out.println("\nThere are no current accounts in the bank.\n" +
-                                            "Please open an account.");
+                                "Please open an account.");
                     }
                     break;
-                case "Q": //Quit
-                    //end the program
+                case "Q": // Quit
+                    // End the program
                     quit = true;
                     System.out.println("\nGoodbye!");
                     break;
@@ -128,23 +170,23 @@ public class Bank {
         } while(!quit);
     }
 
-    //Print the menu, takes index of currently selected account
-    static void printMenu(int currentIndex) {
-        //display menu
-        //if index is not -1 display the account information
+    // Print the menu, takes index of currently selected account
+    public static void printMenu(int currentIndex) {
+        // Display menu
+        // If index is not -1 display the account information
         System.out.println("\nO: Open account\n" +
-                        "D: Deposit\n" +
-                        "S: Select account\n" +
-                        "C: Close account\n" +
-                        "W: Withdraw\n" +
-                        "L: List all accounts\n" +
-                        "Q: Quit\n");
+                "D: Deposit\n" +
+                "S: Select account\n" +
+                "C: Close account\n" +
+                "W: Withdraw\n" +
+                "L: List all accounts\n" +
+                "Q: Quit\n");
 
         System.out.print("current account selected: ");
 
         if (currentIndex != -1) {
             System.out.println(accounts[currentIndex].getAcc() +
-                               "\tBalance: " + moneyFormat.format(accounts[currentIndex].getBalance()));
+                    "\tBalance: " + moneyFormat.format(accounts[currentIndex].getBalance()));
         }
         else {
             System.out.println("NONE");
@@ -154,12 +196,12 @@ public class Bank {
     }
 
 
-    static BankAccount openAcc(int index) {
-        //Grab account number
-        //validate for duplicate account number
-        //Grab balance
-        //create new account and return the object so that the array index can point to the
-        //newly created object
+    public static BankAccount openAcc(int index) {
+        // Grab account number
+        // Validate for duplicate account number
+        // Grab balance
+        // Create new account and return the object so that the array index can point to the
+        // newly created object
         int newAccNum;
         double initialBalance;
         boolean duplicate;
@@ -192,8 +234,8 @@ public class Bank {
         return accounts[index];
     }
 
-    static BankAccount[] resize() {
-        //resize array. Double the size
+    public static BankAccount[] resize() {
+        // Resize array. Double the size
         BankAccount[] temp = new BankAccount[2 * noOfAccs];
 
         System.arraycopy(accounts, 0, temp, 0, noOfAccs);
@@ -203,8 +245,8 @@ public class Bank {
         return accounts;
     }
 
-    static void listAccounts() {
-        //Go through all the accounts using a for loop and display their content
+    public static void listAccounts() {
+        // Go through all the accounts using a for loop and display their content
         double total = 0;
 
         System.out.println("");
@@ -219,8 +261,8 @@ public class Bank {
         System.out.println("\nTotal Bank assets: " + moneyFormat.format(total));
     }
 
-    static int selectAcc(int currentIndex) {
-        //ask for the account number, check to see if it exists and return index
+    public static int selectAcc(int currentIndex) {
+        // Ask for the account number, check to see if it exists and return index
         int accNum;
         boolean foundAcc = false;
 
@@ -245,56 +287,14 @@ public class Bank {
         return currentIndex;
     }
 
-    static void closeAcc(int currentIndex) {
-        //move last account to the index that needs to be deleted
-        //set last account to null
-        //decrement noOfAccts variable
+    public static void closeAcc(int currentIndex) {
+        // Move last account to the index that needs to be deleted
+        // Set last account to null
+        // Decrement noOfAccts variable
         if (noOfAccs > 1) {
             accounts[currentIndex] = accounts[noOfAccs - 1];
             accounts[noOfAccs - 1] = null;
         }
         --noOfAccs;
-    }
-}
-
-class BankAccount {
-    private int accNbr;
-    private double balance;
-
-    BankAccount(int accNbr, double balance) {
-        //set instance variables
-        this.accNbr = accNbr;
-        this.balance = balance;
-    }
-
-    int getAcc() {
-        //return accountNumber
-        return accNbr;
-    }
-
-    double getBalance() {
-        //return balance
-        return balance;
-    }
-
-    void deposit() {
-        //add to deposit
-        double deposit;
-        System.out.print("Enter amount of deposit: ");
-        deposit = Bank.scnr.nextDouble();
-        balance += deposit;
-    }
-
-    void withdraw () {
-        //withdraw as long as there is still $1 in the account
-        double withdraw;
-        System.out.print("Enter amount to withdraw: ");
-        withdraw = Bank.scnr.nextDouble();
-        while ((balance - withdraw) < 1) {
-            System.out.println("\nYou are withdrawing too much: try again");
-            System.out.print("\nEnter amount to withdraw: ");
-            withdraw = Bank.scnr.nextDouble();
-        }
-        balance -= withdraw;
     }
 }
